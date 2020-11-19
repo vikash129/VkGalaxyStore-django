10a655ea2015ee0d19a54cf688a6eb43db263d1d
vik-ash <vikashvermacom92@gmail.com>
2020-11-19 23:57:07 +0530

    first

--- shop/models.py
+++ shop/models.py
@@ -7,7 +7,7 @@ class Product(models.Model):
     prod_id = models.AutoField
     prod_name = models.CharField(max_length=50)
     prod_price = models.IntegerField(default=0)
-    catagery = models.CharField(max_length=50,default='')
+    category = models.CharField(max_length=50,default='')
     sub_category = models.CharField(max_length=50,default='')
     desc = models.CharField(max_length=200,default='')
     prod_date = models.DateField()
--- shop/views.py
+++ shop/views.py
@@ -12,8 +12,20 @@ def home(request):
     n = len(products)
     nSlide = n//4 + ceil( (n/4) - (n//4) )
 
-    params = {'no_of_slides' : nSlide , 'range':range(nSlide) , 'product':products}
-    print(params)
+    allProds = []
+    catProds = Product.objects.values('category' , 'id')
+    cats = { item['category'] for item in catProds}
+
+    for cat in cats:
+        prod = Product.objects.filter(category = cat)
+        allProds.append([ prod , range(1, nSlide) , nSlide ])
+
+    # 1) params = {'no_of_slides' : nSlide , 'range':range(nSlide) , 'product':products}
+    # allProds = [ [products ,range(1,nSlide) , nSlide] ,  [products ,range(1,nSlide) , nSlide] ]
+    params = {'allProds' : allProds}
+    # print(allProds)
+    # print(allProds[1])
+    #  print('nslide :', nSlide , 'n :',n)
 
     return render(request,'shop/home.html',params)
 
